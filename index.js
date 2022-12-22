b4cce1f6cb27cf61294b8994895ef7553b0e22a2
Prakash Banjade <prakashbanjade191@gmail.com>
2022-12-23 01:36:53 +0545

    Fixed issues - wpm and acc

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// console.log('loaded')
 let random_text;
 let ranTextSpan;
 async function getRanQuote() {
-  let response = await fetch("http://quotable.io/random?minLength=100");
+  let response = await fetch("http://quotable.io/random?maxLength=300");
   let data = await response.json();
   random_text = data.content;
 
@@ -18,31 +17,31 @@ async function getRanQuote() {
 
   ranTextSpan = Array.from(document.querySelectorAll(".letter"));
   ranTextSpan[0].scrollIntoView();
-
-  console.log(data.content);
 }
 
 getRanQuote();
 
 let text_box = document.getElementById("text_box");
+text_box.focus();
+
 let startTime;
 let endTime;
 let timeTaken;
 let wrongCount = 0;
 let rightCount = 0;
-// text_box.value = 'this is textbox'
+let second = 0;
+let totalKeysPressed = 0;
 
 text_box.addEventListener("input", checkText);
 
-function addEventForTimer(){
-    text_box.addEventListener(
-      "keypress",
-      () => {
-        // console.log('changed')
-        startTime = new Date();
-      },
-      { once: true }
-    );
+function addEventForTimer() {
+  text_box.addEventListener(
+    "keypress",
+    () => {
+      startTime = new Date();
+    },
+    { once: true }
+  );
 }
 
 addEventForTimer();
@@ -51,7 +50,7 @@ let curr_text;
 let curr_letter;
 let curr_ind;
 
-function checkText() {
+function checkText(e) {
   curr_text = text_box.value;
 
   // getting current letter index
@@ -60,18 +59,7 @@ function checkText() {
   // getting the typed character
   if (curr_text.length > 0) curr_letter = curr_text.charAt(curr_ind);
 
-  if (curr_text.length == ranTextSpan.length) {
-    endTime = new Date();
-
-    timeTaken = (endTime - startTime) / 1000 / 60;
-    let totalWords = ranTextSpan.length / 5;
-    let acc = Math.floor((rightCount / (rightCount + wrongCount)) * 100);
-    let wpm = Math.floor(acc / 100 * (totalWords / timeTaken));
-
-
-    setScore(wpm, acc);
-
-  }
+  if (e.keyCode != 8 || e.keyCode != 46) totalKeysPressed++;
 
   if (ranTextSpan[curr_ind] != undefined) {
     ranTextSpan[curr_ind].scrollIntoView();
@@ -80,7 +68,9 @@ function checkText() {
   // checking the character
   if (ranTextSpan[curr_ind - 1] != undefined)
     ranTextSpan[curr_ind - 1].classList.remove("currSpan");
-  ranTextSpan[curr_ind + 1].classList.remove("currSpan");
+
+  if (ranTextSpan[curr_ind + 1] != undefined)
+    ranTextSpan[curr_ind + 1].classList.remove("currSpan");
 
   let spanText =
     ranTextSpan[curr_ind] != undefined ? ranTextSpan[curr_ind].textContent : "";
@@ -89,23 +79,35 @@ function checkText() {
     ranTextSpan[curr_ind].classList.add("right");
     ranTextSpan[curr_ind].classList.remove("wrong");
 
-    ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
+    if (ranTextSpan[curr_ind + 1] != undefined)
+      ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
   } else {
     wrongCount++;
     ranTextSpan[curr_ind].classList.add("wrong");
     ranTextSpan[curr_ind].classList.remove("right");
 
-    ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
+    if (ranTextSpan[curr_ind + 1] != undefined)
+      ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
+  }
+
+  if (curr_text.length == ranTextSpan.length) {
+    endTime = new Date();
+
+    timeTaken = (endTime - startTime) / 1000 / 60;
+    let acc = Math.floor((rightCount / totalKeysPressed) * 100);
+    let wpm = Math.floor((acc / 100) * (totalKeysPressed / 5 / timeTaken));
+
+    setScore(wpm, acc);
   }
 }
 
-function setScore(wpm, acc){
-    document.getElementById(
-        "wpm"
-      ).innerHTML = `${wpm} WPM <br> <span>Words Per Minute</span>`;
-      document.getElementById(
-        "acc"
-      ).innerHTML = `${acc}% <br> <span>Accuracy</span>`;
+function setScore(wpm, acc) {
+  document.getElementById(
+    "wpm"
+  ).innerHTML = `${wpm} WPM <br> <span>Words Per Minute</span>`;
+  document.getElementById(
+    "acc"
+  ).innerHTML = `${acc}% <br> <span>Accuracy</span>`;
 }
 
 let resetBtn = document.getElementById("reset");
@@ -115,10 +117,12 @@ resetBtn.addEventListener("click", reset);
 function reset() {
   getRanQuote();
   text_box.value = "";
+  text_box.focus();
 
   wrongCount = 0;
   rightCount = 0;
+  totalKeysPressed = 0;
 
-  setScore(0, 0)
+  setScore(0, 0);
   addEventForTimer();
 }
