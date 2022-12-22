bad5325162e5e3078d5317d8815d93e387193030
Prakash Banjade <prakashbanjade191@gmail.com>
2022-12-23 00:42:15 +0545

    Added WPM and accuracy score

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let random_text;
 let ranTextSpan;
 async function getRanQuote() {
-  let response = await fetch("http://quotable.io/random?minLength=200");
+  let response = await fetch("http://quotable.io/random?minLength=100");
   let data = await response.json();
   random_text = data.content;
 
@@ -25,9 +25,27 @@ async function getRanQuote() {
 getRanQuote();
 
 let text_box = document.getElementById("text_box");
+let startTime;
+let endTime;
+let timeTaken;
+let wrongCount = 0;
+let rightCount = 0;
 // text_box.value = 'this is textbox'
 
 text_box.addEventListener("input", checkText);
+
+function addEventForTimer(){
+    text_box.addEventListener(
+      "keypress",
+      () => {
+        // console.log('changed')
+        startTime = new Date();
+      },
+      { once: true }
+    );
+}
+
+addEventForTimer();
 
 let curr_text;
 let curr_letter;
@@ -42,6 +60,19 @@ function checkText() {
   // getting the typed character
   if (curr_text.length > 0) curr_letter = curr_text.charAt(curr_ind);
 
+  if (curr_text.length == ranTextSpan.length) {
+    endTime = new Date();
+
+    timeTaken = (endTime - startTime) / 1000 / 60;
+    let totalWords = ranTextSpan.length / 5;
+    let acc = Math.floor((rightCount / (rightCount + wrongCount)) * 100);
+    let wpm = Math.floor(acc / 100 * (totalWords / timeTaken));
+
+
+    setScore(wpm, acc);
+
+  }
+
   if (ranTextSpan[curr_ind] != undefined) {
     ranTextSpan[curr_ind].scrollIntoView();
     ranTextSpan[curr_ind].classList.add("currSpan");
@@ -54,14 +85,40 @@ function checkText() {
   let spanText =
     ranTextSpan[curr_ind] != undefined ? ranTextSpan[curr_ind].textContent : "";
   if (spanText == curr_letter) {
+    rightCount++;
     ranTextSpan[curr_ind].classList.add("right");
     ranTextSpan[curr_ind].classList.remove("wrong");
 
     ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
   } else {
+    wrongCount++;
     ranTextSpan[curr_ind].classList.add("wrong");
     ranTextSpan[curr_ind].classList.remove("right");
 
     ranTextSpan[curr_ind + 1].classList.remove("right", "wrong");
   }
+}
+
+function setScore(wpm, acc){
+    document.getElementById(
+        "wpm"
+      ).innerHTML = `${wpm} WPM <br> <span>Words Per Minute</span>`;
+      document.getElementById(
+        "acc"
+      ).innerHTML = `${acc}% <br> <span>Accuracy</span>`;
+}
+
+let resetBtn = document.getElementById("reset");
+
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  getRanQuote();
+  text_box.value = "";
+
+  wrongCount = 0;
+  rightCount = 0;
+
+  setScore(0, 0)
+  addEventForTimer();
 }
